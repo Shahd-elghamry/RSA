088b3d4b7a4259938b86d36550a0655c9d730b68
Shahd Elghamry <139113340+Shahd-elghamry@users.noreply.github.com>
2024-04-19 00:48:25 +0200

    Update program 2

--- Program_2.py
+++ Program_2.py
@@ -1,8 +1,7 @@
+
 import random 
 import math 
 import time
-# from rsa import C
-# from rsa import M
 
 def is_prime(n):
     if n < 2:
@@ -19,25 +18,34 @@ def generate_prime(bits):
             num += 1
         if is_prime(num): 
             return num
-        
+
 n = int(input("Enter the number to factorize: "))
 e = int(input("Enter public key(e): "))
-
+encrypted = int(input("Enter encrypted: "))
+message = int(input("Enter decrypted: "))
+decrypted = message
+bits = int(input("Number of bits 8/16: "))
 start_time = time.time()
-
 p, q = 0, 0
 while p * q != n:
-    p = generate_prime(n)
+    if bits==8:
+        p = generate_prime(8)  # Adjust the number of bits as needed
+    elif bits==16:
+        p = generate_prime(16) 
+    else:
+        exit() 
     q = n // p
-
 phi_n = (q - 1) * (p - 1)
 
-def find_priv(e, phi_n):
+def find_priv(e, phi_n, encrypted, message):
     for d in range(2, phi_n):
         if (e * d) % phi_n == 1: 
-            return d
-        
-d = find_priv(e, phi_n)
+            decrypted = pow(encrypted, d, n)
+            if decrypted == message:
+                return d
+    return None
+
+d = find_priv(e, phi_n, encrypted, message)
 print("Private key (d): ", d)
 
 exact_time = time.time() - start_time 
